4a515888862c4f9133ad90e0151157f477b0fbd3
TmRichrd <1084491297@qq.com>
2023-05-22 07:58:29 +0000

    Deploying to gh-pages from @ TmRichrd/workflow-test@f3cf5d5101f455e81c8a4f842d07cdf6d4e696ff 🚀

--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import './style.css'
-// import App from './App'
-import Home from './Home'
-import i18n from "./i18n/index.ts"
-const app = createApp(Home)
-app.use(i18n)
-app.mount('#yjzz-app')
